28e77f26a8b3f02b599c8f3facb5100700560ac2
Arnbiorn <skrutelev988@gmail.com>
2021-01-09 17:31:47 +0300

    Added local and prod database properties

---
+++ tickets/prod_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'irqjgvyr',
+        'USER': 'irqjgvyr',
+        'PASSWORD': '8Rsh1Qq8rvTmoDicTxlQXRnX8m-_QP0b',
+        'HOST': 'rogue.db.elephantsql.com',
+        'PORT': '5432',
+    }
+}
