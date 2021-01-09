a1f1b9ae067c1b625dfab03b389834a5b394d18b
Arnbiorn <skrutelev988@gmail.com>
2021-01-10 00:02:49 +0300

    Fix some problems

--- tickets/settings.py
+++ tickets/settings.py
@@ -71,12 +71,16 @@ WSGI_APPLICATION = 'tickets.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'irqjgvyr',
+        'USER': 'irqjgvyr',
+        'PASSWORD': '8Rsh1Qq8rvTmoDicTxlQXRnX8m-_QP0b',
+        'HOST': 'rogue.db.elephantsql.com',
+        'PORT': '5432',
+    }
+}
 
 
 
